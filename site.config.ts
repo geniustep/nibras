ea5f6a31f38cfad8920cb6cdf26105e466981f63
geniustep <agsteps@gmail.com>
2026-05-20 22:00:22 +0100

    first commit

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
     en: "Madaris Nibras",
   },
   whatsappNumber: "212XXXXXXXXX",
-  email: "contact@madarisnibras.org",
+  email: "contact@madarisnibras.ma",
   phone: "PUT_PHONE_HERE",
   googleMapsUrl:
     "https://www.google.com/maps/place/Madaris+Nibras/@35.7374807,-5.8696778,17z/data=!3m1!4b1!4m6!3m5!1s0xd0b871d0a9cb015:0x2def6a6b15a9186f!8m2!3d35.7374807!4d-5.8696778!16s%2Fg%2F11nc3m858f?hl=ar&entry=ttu&g_ep=EgoyMDI2MDUxMy4wIKXMDSoASAFQAw%3D%3D",
@@ -15,7 +15,8 @@ export const siteConfig = {
     youtube: "PUT_YOUTUBE_CHANNEL_URL_HERE",
   },
   assets: {
-    logo: "/logo.png",
+    // logo: "/logo.png",
+    logo: "/logo_nibrass.png",
     mainImage: "/main.jpeg",
   },
 };
