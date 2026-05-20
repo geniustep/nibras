e9b4308df7dcbf0f9ccae3891242925e55a69076
geniustep <agsteps@gmail.com>
2026-05-20 21:48:59 +0100

    first commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,8 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./src/i18n/routing";
+import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: [
-    "/((?!_next|_vercel|.*\\..*).*)",
-  ],
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
